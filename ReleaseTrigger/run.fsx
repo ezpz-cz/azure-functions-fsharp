eebbf8951bb6f1916ef86d4d71d9652504e82e1a
jakubsuchybio <dev@jakubsuchy.com>
2016-09-07 23:49:22 +0200

    Test

--- ReleaseTrigger/run.fsx
+++ ReleaseTrigger/run.fsx
@@ -3,6 +3,7 @@ open System.Net
 open System.Net.Http
 open System.Net.Http.Headers
 open System.Web
+open FSharp.Data
 
 let Run (req: HttpRequestMessage) (log: TraceWriter) = async {
     let! content = req.Content.AsyncReadAsString ()
