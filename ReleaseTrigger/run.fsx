c2d344109e6278437f68089df3aad460fbec4f83
jakubsuchybio <dev@jakubsuchy.com>
2016-09-07 23:45:39 +0200

    Test

--- ReleaseTrigger/run.fsx
+++ ReleaseTrigger/run.fsx
@@ -3,7 +3,7 @@ open System.Net
 open System.Net.Http
 
 let Run (req: HttpRequestMessage) (log: TraceWriter) = async {
-    let! content = req.Content.AsyncReadAsString()
+    let! content = req.Content.AsyncReadAsString ()
     return req HttpStatusCode.OK
            <| ``Content-Type`` "text/plain"
            <| new StringContent(content)
